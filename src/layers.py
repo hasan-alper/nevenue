fb90d16fe792f61c1601fd298fde9da1b78c5462
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2023-12-18 00:11:47 +0300

    implement relu and softmax activation layers

--- src/layers.py
+++ src/layers.py
@@ -25,3 +25,35 @@ class Dense():
         print(f"Weights:\n{self.weights}\n")
         print(f"Biases:\n{self.biases}\n")
         print(f"Outputs:\n{self.outputs}\n")
+
+
+class ReLU():
+    """
+    Rectified Linear Unit activation layer class.
+
+    It returns element-wise `max(0, x)`.
+    """
+
+    def forward(self, inputs: list) -> None:
+        self.inputs = np.array(inputs)
+        self.outputs = np.maximum(0, self.inputs)
+
+    def info(self) -> None:
+        print(f"Inputs:\n{self.inputs}\n")
+        print(f"Outputs:\n{self.outputs}\n")
+
+
+class Softmax():
+    """
+    Softmax activation layer class.
+
+    Softmax converts a vector of values to a probability distribution. It returns `exp(x) / sum(exp(x))`.
+    """
+
+    def forward(self, inputs: list) -> None:
+        self.inputs = np.array(inputs)
+        self.outputs = np.exp(self.inputs) / np.sum(np.exp(self.inputs))
+
+    def info(self) -> None:
+        print(f"Inputs:\n{self.inputs}\n")
+        print(f"Outputs:\n{self.outputs}\n")
